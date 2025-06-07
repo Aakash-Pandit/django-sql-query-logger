e99af4732a4b1789b6705464af97088e25e5f28f
Aakash-Pandit <aakashpandit366@gmail.com>
2025-06-07 16:13:00 +0530

    updates bash file

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,6 @@ rm -rf dist/ build/ *.egg-info/
 echo "Building package..."
 python -m build
 
-# if [ ! -f ./.pypirc ]; then
-#     echo "Error: ./.pypirc not found. Please create it with your API tokens."
-#     echo "See: https://packaging.python.org/en/latest/specifications/pypirc/"
-#     exit 1
-# fi
-
-# echo "Uploading to Test PyPI..."
-# python -m twine upload --repository testpypi dist/* --verbose
-
-# echo "Package uploaded to Test PyPI!"
-# echo "Test installation with:"
-# echo "pip install --index-url https://test.pypi.org/simple/ django-sql-query-logger"
-
 read -p "Upload to real PyPI? (y/N): " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
